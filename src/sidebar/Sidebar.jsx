845c9c5f257ba7f0148f0936c1e47c2fb8769253
Aditi Jain <60309617+aditijain816@users.noreply.github.com>
2021-06-19 23:16:54 +0530

    continuing homepage

---
+++ src/sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import "./sidebar.css"
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarItem">
+                <span className="sidebarTitle">ABOUT ME</span>
+                <img src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg" alt="" />
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut similique aspernatur in, quam illo perferendis nesciunt repellendus totam ullam natus!
+                </p>
+            </div>
+        </div>
+    )
+}
