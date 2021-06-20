ac5c1eed69d6e9b1160ad737d6789c05908ac03c
Aditi Jain <60309617+aditijain816@users.noreply.github.com>
2021-06-20 18:02:32 +0530

    sidebar

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -10,6 +10,26 @@ export default function Sidebar() {
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut similique aspernatur in, quam illo perferendis nesciunt repellendus totam ullam natus!
                 </p>
             </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">CATEGORIES</span>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">Life</li>
+                    <li className="sidebarListItem">Music</li>
+                    <li className="sidebarListItem">Style</li>
+                    <li className="sidebarListItem">Sport</li>
+                    <li className="sidebarListItem">Tech</li>
+                    <li className="sidebarListItem">Cinema</li>
+                </ul>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">FOLLOW US</span>
+                <div className="sidebarSocial">
+                    <i className="sidebarIcon fab fa-facebook-square"></i>
+                    <i className="sidebarIcon fab fa-twitter-square"></i>
+                    <i className="sidebarIcon fab fa-pinterest-square"></i>
+                    <i className="sidebarIcon fab fa-instagram-square"></i>
+                </div>
+            </div>
         </div>
     )
 }
